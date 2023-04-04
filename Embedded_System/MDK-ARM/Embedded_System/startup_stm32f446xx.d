11b99e4a2c4b16195de1e1195c27e8803949ba0c
nhattuong <tuongdotn@gmail.com>
2023-04-04 13:08:21 +0700

    Upload Embedded System

---
+++ Embedded_System/MDK-ARM/Embedded_System/startup_stm32f446xx.d
@@ -0,0 +1,1 @@
+embedded_system\startup_stm32f446xx.o: startup_stm32f446xx.s
---
+++ Embedded_System/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Embedded_System' 
+ * Target:  'Embedded_System' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
