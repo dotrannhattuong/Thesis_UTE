8e244b63bc25aa3439b2d48ca2fb9447f11c58b5
dotrannhattuong <tuongdotn@gmail.com>
2023-04-05 22:27:46 +0700

    Update code

--- Embedded_System/Core/Src/main.c
+++ Embedded_System/Core/Src/main.c
@@ -90,9 +90,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_USART2_UART_Init();
   MX_TIM1_Init();
   MX_TIM3_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 	/************** TIMER **************/
 	HAL_TIM_Base_Start_IT(&htim1);
